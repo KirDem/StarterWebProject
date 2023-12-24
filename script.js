f51a735ce017bd1679bca458b3e4311f5c1caff3
Kirill Dmechenko <k1ridemch@yandex.ru>
2023-12-24 17:30:08 +0300

    js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//123123
+$(document).ready(function() {
+    $('.reviews-slider').slick({
+      infinite: true,
+      speed: 500,
+      prevArrow: $("#reviews-previous"),
+      nextArrow: $("#reviews-next"),
+      fade: true,
+      swipe: false, 
+      draggable: false,
+      slidesToShow: 1,
+      adaptiveHeight: true
+    });
+    
+    $('.reviews-slider').on('afterChange', function(event, slick, currentSlide) {
+      $('#reviews-number').text('0' + (currentSlide + 1));
+    });
+
+}
