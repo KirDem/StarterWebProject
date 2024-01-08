03a4dd64e9dc35c742ac2d7bcb2cee020c0c6517
potapovadd <144013043+potapovadd@users.noreply.github.com>
2024-01-08 14:06:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,131 @@ $(document).ready(function() {
   $('.reviews-slider').on('afterChange', function(event, slick, currentSlide) {
     $('#reviews-number').text('0' + (currentSlide + 1));
   });
+
+  //First slider
+  $("#wwu-slider-1").slick({
+    infinite: true,
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '10%',
+    autoplaySpeed: 3500,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {slidesToShow: 3}
+        },
+        {
+            breakpoint: 600, 
+            settings: {slidesToShow: 2}
+        }
+    ]
+  });
+
+    //Second slider
+  $("#wwu-slider-2").slick({
+    infinite: true,
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: '10%',
+    autoplaySpeed: 3500,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {slidesToShow: 3}
+        },
+        {
+            breakpoint:600, 
+            settings: {slidesToShow: 2}
+        }
+    ]
+  });  
+
+   $(".faq-enum div:first").addClass("active");
+  $(".faq-enum p:not(:first)").hide();
+  $(".faq-enum h3").click(function () {
+    if (!$(this).parent().hasClass("active")) {
+      $(".faq-enum p:visible").slideUp("fast");
+      $(this).next("p").slideToggle("fast");
+      $(".faq-enum div").removeClass("active");
+      $(this).parent().toggleClass("active");
+    }
+    else {
+      $(".faq-enum p:visible").slideUp("fast");
+      $(".faq-enum div").removeClass("active");
+    }
+  });
+});
+
+//Send form
+function saveLocalStorage() {
+  localStorage.setItem("inputName", $("#footer-name").val());
+  localStorage.setItem("inputNumber", $("#footer-number").val());
+  localStorage.setItem("inputEmail", $("#footer-email").val());
+  localStorage.setItem("inputMessage", $("#footer-message").val());
+  localStorage.setItem("footer-policy", $("#footer-policy").prop("checked"));
+}
+
+function loadLocalStorage() {
+  if (localStorage.getItem("name") !== null)
+      $("#footer-name").value(localStorage.getItem("inputName"));
+  if (localStorage.getItem("number") !== null)
+      $("#footer-number").value(localStorage.getItem("inputNumber"));
+  if (localStorage.getItem("email") !== null)
+      $("#footer-email").value(localStorage.getItem("inputEmail"));
+  if (localStorage.getItem("message") !== null)
+      $("#footer-message").value(localStorage.getItem("inputMessage"));
+  if (localStorage.getItem("policy") !== null) {
+      $("input.chb").prop("checked", localStorage.getItem("footer-policy") === "true");
+      if ($("input.chb").prop("checked"))
+          $("input.footer-contact-button").removeAttr("disabled");
+  }
+}
+function clear() {
+  localStorage.clear()
+  $("#footer-name").val("");
+  $("#footer-number").val("");
+  $("#footer-email").val("");
+  $("#footer-message").val("");
+  $("input.chb").val(false);
+}
+
+$(document).ready(function () {
+  loadLocalStorage();
+  $("#footer-form").submit( async function (e) {
+      e.preventDefault();
+      let data = $(this).serialize();
+      console.log(data)
+      await fetch('https://formcarry.com/s/adu1AdDRs76', {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: {
+              'Content-Type': 'application/json; charset=UTF-8',
+          }
+      })
+          .then((response) => response.json())
+          .then((data) => {
+              console.log(data)
+              alert("form send")
+          })
+          .catch((e) => {
+              alert('error')
+          })
+  });
+
+  $("input.chb").change(function () {
+    if ((!this.checked))
+        $("input.footer-contact-button").attr("disabled", "");
+    else
+        $("input.footer-contact-button").removeAttr("disabled");
+})
+
+$("#footer-form").change(saveLocalStorage);
+})                
